60dbb07bfb776496791bc0c4557b4ec57e552eac
Justin Balagtas <justinseebee@hotmail.com>
2023-02-16 13:45:30 -0500

    replace with async and await

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const img = document.querySelector('img');
 const btn = document.querySelector('.btn');
 
-function useAPI(srch) {
-    console.log(srch);
-   
+async function useAPI(search) {
+    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=fs7gQxJQBTG4pQAjolZNSES8hBWr14Om&s=${search}`, {mode: 'cors'});
+    const searchData = await response.json();
+    img.src = searchData.data.images.original.url;
 }
 
 // Button event listener
@@ -11,8 +12,14 @@ btn.addEventListener('click', () => {
     const search = document.querySelector('#search');
     console.log(search.value);
     const val = search.value;
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=fs7gQxJQBTG4pQAjolZNSES8hBWr14Om&s=${val}`, {mode: 'cors'})
-    .then((response) => response.json())    // Returns response object => Then to extract JSON from response body using json();
-    .then((response) => img.src = response.data.images.original.url);   // Returns 2nd promise containing response body as JSON
+
+    useAPI(val);
+    
 })
+
+
+
+// fetch(`https://api.giphy.com/v1/gifs/translate?api_key=fs7gQxJQBTG4pQAjolZNSES8hBWr14Om&s=${val}`, {mode: 'cors'})
+//     .then((response) => response.json())    // Returns response object => Then to extract JSON from response body using json();
+//     .then((response) => img.src = response.data.images.original.url);   // Returns 2nd promise containing response body as JSON
 
